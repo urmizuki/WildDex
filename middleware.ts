cb8aaa51f2963cae79a06f569891d52d1e16bf34
urmizuki <urmizuki@users.noreply.github.com>
2026-06-07 04:24:07 +0800

    fix: remove middleware to stop Vercel invocation errors

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { type NextRequest, NextResponse } from 'next/server'
-
-export async function middleware(request: NextRequest) {
-  return NextResponse.next({ request })
-}
-
-export const config = {
-  matcher: ['/', '/index.html', '/login'],
-}
